866e1cf4ae82129c1dcab8153aa677b39eaf9440
reuben <reuben.eggar@me.com>
2016-12-03 10:30:34 +0000

    🎄Day 3 part 1

---
+++ 2016/3.js
@@ -0,0 +1,22 @@
+export default input =>
+  input
+  // Split input into lines
+  .split('\n')
+  // Filter out invalid triangles
+  .filter(line => {
+    const [sideOne, sideTwo, sideThree] =
+    line
+    // Split by whitespace
+    .split(' ')
+    // Remove empty strings from array due to splitting
+    .filter(s => s)
+    // Cast to intergers
+    .map(d => +d)
+    // Order side lengths accendingly
+    .sort((a, b) => a - b)
+
+    // If the sum of the two smaller lengths is bigger than the larger side then we have a valid triangle
+    return sideOne + sideTwo > sideThree
+  })
+  // Return number of valid triangles
+  .length
