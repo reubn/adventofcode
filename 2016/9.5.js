2233a841d5699c5c628a8e2977f1a4daaf0a8ddf
reuben <reuben.eggar@me.com>
2016-12-09 19:19:34 +0000

    🎄 Day 9 part 2

---
+++ 2016/9.5.js
@@ -0,0 +1,33 @@
+const decompress = input => {
+  let length = 0
+
+  // While there are still characters in the data
+  while(input.length){
+    // If we have not found a marker
+    if(!input.startsWith('(')){
+      // Increase the length of the decompressed data
+      length += 1
+
+      // Remove the affected data from the string
+      input = input.substring(1)
+    } else {
+      // Otherwise, we must have a marker
+
+      // Extract the marker from the data
+      const marker = input.substring(0, input.indexOf(')') + 1)
+
+      // Extract the reach of the marker and the number of times to repeat the string
+      const [reach, times] = marker.slice(1, -1).split('x')
+
+      // Increase the length of the decompressed data by the length of the decompressed region multiplied by the number of times to repeat
+      length += decompress(input.substr(marker.length, reach)) * times
+
+      // Remove the affected data from the string
+      input = input.substring(marker.length + +reach)
+    }
+  }
+
+  return length
+}
+
+export default decompress
