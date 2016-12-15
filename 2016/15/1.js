59d104ea307ac4a3970aac639dee151547d45549
reuben <reuben.eggar@me.com>
2016-12-15 21:04:22 +0000

    🎄 Day 15 part 1

---
+++ 2016/15/1.js
@@ -0,0 +1,24 @@
+export default input => {
+  let discs = input
+  // Split input into lines
+  .split('\n')
+  // Convert to disc
+  .map(line => {
+    const [, total, start] = /has (\d+) positions; at time=\d+, it is at position (\d+)/.exec(line)
+    return {total, start: +start}
+  })
+
+  // Just need something to increment, can seemingly be any integer....strange
+  let time = 160589
+  // While not every disc is in position `0`
+  while(!discs.every(({position=1}) => !position)){
+    // Increment time
+    time++
+
+    // Increase position of discs each 'tick'
+    discs = discs.map(({total, start, position=start}, index) => ({total, start, position: (start + time + index) % total}))
+  }
+
+  // return time. Minus 1 as check occurs after increment
+  return time - 1
+}
