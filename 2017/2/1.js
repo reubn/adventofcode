0be1ea11954b4ea940b8264ce7762421259e40f5
reuben <reuben.eggar@me.com>
2017-12-02 11:03:12 +0000

    ✨ Casting is Unnecessary

--- 2017/2/1.js
+++ 2017/2/1.js
@@ -1,8 +1,6 @@
 export default input =>
   input.split('\n') // Split into lines
     .reduce((total, line) => {
-        .reduce((array, part) => part ? [...array, +part] : array, []) // Filter out empty elements where multiple whitespace characters existed, then cast to number
-
       const parts = line.split(/\s+/) // Split by whitespace to get at numbers
 
       return total + (Math.max(...parts) - Math.min(...parts)) // Add difference between highest and lowest values to cummulative total
--- 2017/2/2.js
+++ 2017/2/2.js
@@ -1,7 +1,6 @@
 export default input =>
   input.split('\n') // Split into lines
     .reduce((total, line) => {
-        .reduce((array, part) => part ? [...array, +part] : array, []) // Filter out empty elements where multiple whitespace characters existed, then cast to number
       const parts = line.split(/\s+/) // Split by whitespace to get at numbers
 
       let lineResult = 0
